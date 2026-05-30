e3748084a1c5de066fbb6613ef5100c0a46fa8ae
kushal malla1 <kushal.mallaf25@techspire.edu.np>
2026-05-30 16:31:11 +0545

    Set up CI/CD

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,17 @@ function App() {
     <div className="app">
       <div className="card">
         <h1>Hello from CI/CD!</h1>
-        <p>This React app is automatically deployed to EC2 via GitHub Actions.</p>
-        <p>Push any change to the <code>main</code> branch and watch it go live.</p>
+        <p>
+          This React app is automatically deployed to EC2 via GitHub Actions.I'm
+          changing this statement for practise.
+        </p>
+        <p>
+          Push any change to the <code>main</code> branch and watch it go live.
+        </p>
         <div className="badge">Techspire College 2026</div>
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
